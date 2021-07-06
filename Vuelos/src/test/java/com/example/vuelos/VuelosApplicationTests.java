1a6f3ba3083a7c38c8b267c11b6877a624c6ae71
Nightmarehxc <melopeico@gmail.com>
2021-07-06 17:37:14 +0200

    initialVuelos

---
+++ Vuelos/src/test/java/com/example/vuelos/VuelosApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.vuelos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuelosApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
