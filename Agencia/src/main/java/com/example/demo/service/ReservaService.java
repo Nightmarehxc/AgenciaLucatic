796e6f25e590716255a013e6d0405fdae7f6167f
alumno <alumno@alumno09>
2021-07-07 16:51:18 +0200

    fix: errors

--- Agencia/src/main/java/com/example/demo/service/ReservaService.java
+++ Agencia/src/main/java/com/example/demo/service/ReservaService.java
@@ -18,17 +18,17 @@ public class ReservaService {
 	public String urlMicroProductorReservas = "http://agencia-micro-reservas/reservas";
     
     public List<Reservas> getAllByUser(String id){
-        Reservas[] reservas = restTemplate.getForObject(urlMicroProductorReservas+"/getAllByUser?id="id, Reservas[].class);
+        Reservas[] reservas = restTemplate.getForObject(urlMicroProductorReservas+"/getAllByUser?id=" + id, Reservas[].class);
         return Arrays.asList(reservas);
     }
 
     public List<Reservas> getAllByHotel(String id){
-        Reservas[] reservas = restTemplate.getForObject(urlMicroProductorReservas+"/getAllByHotel?id="id, Reservas[].class);
+        Reservas[] reservas = restTemplate.getForObject(urlMicroProductorReservas+"/getAllByHotel?id=" + id, Reservas[].class);
         return Arrays.asList(reservas);
     }
 
     public List<Reservas> getAllByVuelo(String id){
-        Reservas[] reservas = restTemplate.getForObject(urlMicroProductorReservas+"/getAllByVuelo?id="id, Reservas[].class);
+        Reservas[] reservas = restTemplate.getForObject(urlMicroProductorReservas+"/getAllByVuelo?id=" + id, Reservas[].class);
         return Arrays.asList(reservas);
     }
 }
